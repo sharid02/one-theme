620ceb5b64e2c3ac4429044cfb4ce4d52a45d472
sharid02 <sharidkayes@gmail.com>
2018-03-04 09:08:16 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+//smoothScroll
+
+
+$(document).ready(function(){
+	$('a').smoothScroll();
+
+});
+
+
+//mobile navbar hide after click
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+})
+
+Open Your Door to The World
+The Most Demanding Career Globally
+The Fastest Growing Industry
+Globally The Safest Working Environment for Woman
+Easy Career Diversification
